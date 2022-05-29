900ff86432fc857df13230c45df1e72dc329b2dd
Turtley12 <76929530+Turtley12@users.noreply.github.com>
2022-05-29 10:34:40 -0700

    Add channel url generation

--- channelselect/channelselect.go
+++ channelselect/channelselect.go
@@ -5,33 +5,72 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"syscall/js"
 )
 
+var channels map[string]string
+
 func CreateButtons() {
-	channels := getChannels()
-	fmt.Println(channels)
+	channels = getChannels()
+	//fmt.Println(channels)
 	document := js.Global().Get("document")
 	selectiondiv := document.Call("getElementById", "selection")
 	fieldset := document.Call("createElement", "fieldset")
+	fieldset.Call("setAttribute", "id", "fieldset")
 
 	for name, _ := range channels {
+		div := document.Call("createElement", "div")
+		div.Call("setAttribute", "class", "button_div")
 		check := document.Call("createElement", "input")
-		check.Call("setAttribute", "type", "radio")
-		check.Call("setAttribute", "name", "channel_slect")
+		check.Call("setAttribute", "type", "checkbox")
+		check.Call("setAttribute", "name", "channel_select")
+		check.Call("setAttribute", "class", "channel_select")
 		check.Call("setAttribute", "value", name)
-		text := document.Call("createTextNode", name)
-		check.Call("appendChild", text)
-		fieldset.Call("appendChild", check)
+		check.Call("setAttribute", "id", strings.ToLower(name))
+
+		label := document.Call("createElement", "label")
+		label.Set("htmlFor", name)
+		label.Call("appendChild", document.Call("createTextNode", name))
+
+		br := document.Call("createElement", "br")
+
+		div.Call("appendChild", check)
+		div.Call("appendChild", label)
+		div.Call("appendChild", br)
+		fieldset.Call("appendChild", div)
 	}
 	selectiondiv.Call("appendChild", fieldset)
 }
 
 func GetUserSelection() {
+	var userchannels []string
+
+	document := js.Global().Get("document")
+	for name, id := range channels {
+		check := document.Call("getElementById", strings.ToLower(name))
+		if check.Get("checked").Bool() {
+			userchannels = append(userchannels, id)
+		}
+	}
+	url := createLink(userchannels)
+	clipboard := js.Global().Get("navigator").Get("clipboard")
+	clipboard.Call("writeText", js.ValueOf(url))
 
 }
 
-func getChannels() map[string]interface{} {
+func createLink(userchannels []string) string {
+	window := js.Global().Get("window")
+	current_url := window.Get("location").Get("href").String()
+	output_url := current_url + "userfeed.html?channels="
+	for _, channel := range userchannels {
+		output_url = output_url + channel + ","
+	}
+	return output_url
+
+}
+
+func getChannels() map[string]string {
 	url := "channels.json"
 	//get response
 	response, err := http.Get(url)
@@ -44,8 +83,8 @@ func getChannels() map[string]interface{} {
 	if err2 != nil {
 		fmt.Println(err2)
 	}
-	fmt.Println(string(bytes))
-	var result map[string]interface{}
+	//fmt.Println(string(bytes))
+	var result map[string]string
 	err1 := json.Unmarshal(bytes, &result)
 	if err1 != nil {
 		fmt.Println(err1)
--- main.go
+++ main.go
@@ -2,31 +2,54 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"syscall/js"
 
 	"github.com/turtley12/antigorithm/channelselect"
 
 	"github.com/turtley12/antigorithm/view"
 
-	//"fmt"
-
 	"github.com/turtley12/antigorithm/feed"
 )
 
 func main() {
+	c := make(chan struct{}, 0)
+
 	pathname := js.Global().Get("window").Get("location").Get("pathname").String()
-	fmt.Println("pathname: " + pathname)
+	//fmt.Println("pathname: " + pathname)
 	if pathname == "/userfeed.html" || pathname == "/userfeed" {
 		userFeed()
 	} else {
 		home()
 	}
+	<-c
 }
 func userFeed() {
-	var userchannels []string
-	userchannels = append(userchannels, "UCSju5G2aFaWMqn-_0YBtq5A")
+	userchannels := getUserChannels()
+
 	view.DisplayFeed(feed.GetUserFeed(userchannels))
 }
+func getUserChannels() []string {
+	window := js.Global().Get("window")
+	location := window.Get("location")
+	search := location.Get("search").String()
+
+	if search == "" {
+		location.Call("replace", location.Get("origin"))
+	}
+
+	params := strings.ReplaceAll(search, "?channels=", "")
+	userchannels := strings.Split(params, ",")
+	for _, channel := range userchannels {
+		fmt.Println(channel)
+	}
+	return userchannels
+}
 func home() {
+	js.Global().Set("generate", js.FuncOf(generate))
 	channelselect.CreateButtons()
+}
+func generate(this js.Value, args []js.Value) interface{} {
+	channelselect.GetUserSelection()
+	return js.ValueOf(nil)
 }
